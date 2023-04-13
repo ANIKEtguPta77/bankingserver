48afebba0a4ece875e8b3f9f3b8612e45fe53d8b
ANIKEtguPta77 <107102866+ANIKEtguPta77@users.noreply.github.com>
2023-04-13 16:51:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 const mongoose=require('mongoose');
 const app=require('./app');
+const PORT = process.env.PORT || 5000;
 
 // mongoose.connect(process.env.MONGODB_URL_LOCAL,{
 //     useNewUrlParser:true,
