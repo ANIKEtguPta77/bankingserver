31f2826405f9c7c7a5bee1ef6ddb0ed6c6adfff1
ANIKEtguPta77 <107102866+ANIKEtguPta77@users.noreply.github.com>
2023-04-13 17:25:31 +0530

    Update userRouter.js

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,10 +1,11 @@
 const router=require('express').Router();
-const { signUp,verifyOtp,login,entry,transactions }=require('../Controllers/userController')
+const { a,signUp,verifyOtp,login,entry,transactions }=require('../Controllers/userController')
 
 router.route('/signup').post(signUp);
 router.route('/signup/verify').post(verifyOtp);
 router.route('/rsignup').post(entry);
 router.route('/login').post(login);
 router.route('/transactions').post(transactions);
+router.route('/a').get(a);
 
 module.exports=router;
