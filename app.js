7a673985afa274e359d7f784680d1e86bd2013a8
ANIKEtguPta77 <107102866+ANIKEtguPta77@users.noreply.github.com>
2023-04-14 11:13:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const cors = require("cors");
 const app=express();
+const bodyParser = require('body-parser');
 const userRouter=require('./Routers/userRouter')
 app.use(express.json());
 app.use(express.urlencoded());
